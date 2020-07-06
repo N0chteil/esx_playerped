a7d90f1900bba92cd6e54670d13f59de624170e5
N0chteil <53608074+N0chteil@users.noreply.github.com>
2020-07-06 18:33:38 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,7 +2,7 @@ ESX = nil
 
 local coord = {
 	{-3137.6, 1125.7, 20.6,"Car-1",360.0,0x3B96F23E,"s_m_y_valet_01"},
-	{Coordinates,"Description",heading,hashcode,"name"}
+	-- {Coordinates,"Description",heading,hashcode,"name"}
 }
     
     Citizen.CreateThread(function()
